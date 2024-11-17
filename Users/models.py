e2211dcb146bc78912bdc7a1b0cdfb0ef7ed210d
amirhossein zinati <amirhosseinzt78@gmail.com>
2024-11-17 16:14:31 +0330

    first commit

---
+++ Users/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+from Users.managers import CustomUserManager
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=False)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class GenderChoices(models.TextChoices):
+        MALE = 'male'
+        FEMALE = 'female'
+        Nonbinary = 'nonbinary'
+        OTHER = 'other'
+
+    name = models.CharField(max_length=50, null=True, blank=False)
+    user_name = models.CharField(max_length=50, null=True, blank=False, unique=True)
+    phone = models.CharField(max_length=11, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    image = models.ImageField(upload_to='user/image/', null=True, blank=True)
+    cover = models.ImageField(upload_to='user/cover/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True, max_length=1000)
+    gender = models.CharField(max_length=50, null=True, blank=True, choices=GenderChoices.choices,
+                              default=GenderChoices.OTHER)
+
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True)
+
+    is_verified = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True)
+    date_joint = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    version = models.CharField(default='1.0.0', max_length=20)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.user_name
---
+++ corci/settings.py
@@ -0,0 +1,102 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-91$!dxtr-f*m6hd&j55%3(etsi37ex0s88u5jjmsl=&9(#+9s-'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'Users',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'corci.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'corci.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+AUTH_USER_MODEL = 'Users.CustomUser'
